12354f0ace79331f7da61ee1aef3ae24f5f6cb18
Dev <dev@terminalit.local>
2026-07-28 17:26:37 +0100

    Clean up build artifacts from repository

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_terminalit_TerminalitApp.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.terminalit.TerminalitApp"
-)
-public final class _com_terminalit_TerminalitApp {
-}
